97c7d1b86c331e66480fa65c3a6df6460f7ff828
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-12-21 00:14:04 +0330

    solid E01 SRP

---
+++ src/com/amirahmadadibi/java/teaching/SRP/good/Book.java
@@ -0,0 +1,19 @@
+package com.amirahmadadibi.java.teaching.SRP.good;
+
+public class Book {
+    private String name;
+    private String author;
+    private String text;
+
+    //constructor, getters and setters
+
+    // methods that directly relate to the book properties
+    public String replaceWordInText(String word, String replacementWord) {
+        return text.replaceAll(word, replacementWord);
+    }
+
+    public boolean isWordInText(String word) {
+        return text.contains(word);
+    }
+
+}
---
+++ src/com/amirahmadadibi/java/teaching/SRP/good/Publisher.java
@@ -0,0 +1,11 @@
+package com.amirahmadadibi.java.teaching.SRP.good;
+
+public class Publisher {
+    void printTextToConsole(){
+        // our code for formatting and printing the text
+    }
+
+    void shareToAnotherMedia(){
+
+    }
+}
