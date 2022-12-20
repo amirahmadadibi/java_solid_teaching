71aafb8c2faec1ca836d2ffd2be5cc1550415d86
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-12-21 00:20:40 +0330

    solid E02 open close principle

---
+++ src/com/amirahmadadibi/java/teaching/OCP/bad/Guitar.java
@@ -0,0 +1,9 @@
+package com.amirahmadadibi.java.teaching.OCP.bad;
+
+public class Guitar {
+    private String make;
+    private String model;
+    private int volume;
+
+    //Constructors, getters & setters
+}
---
+++ src/com/amirahmadadibi/java/teaching/OCP/good/CoolGuitar.java
@@ -0,0 +1,10 @@
+package com.amirahmadadibi.java.teaching.OCP.good;
+
+import com.amirahmadadibi.java.teaching.OCP.bad.Guitar;
+
+public class CoolGuitar extends Guitar {
+
+    void changeColor(){
+
+    }
+}
